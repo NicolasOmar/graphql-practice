f3c59d515a0f8791c4d85867f52a03f666409f06
NicolasOmar <nicolas.passerino@gmail.com>
2022-01-25 21:44:00 -0300

    UT added for queries

--- 4-testing/src/server/tests/queries.test.js
+++ 4-testing/src/server/tests/queries.test.js
@@ -1,20 +1,36 @@
 import 'cross-fetch/polyfill'
 import ApolloBoost from 'apollo-boost'
 // MOCKS
-import { _user } from './mocks.json'
+import { _user, _post, _comment } from './mocks.json'
 // MUTATIONS AND QUERIES FOR TESTING
-import { getUsers } from './testRequests/queries'
+import { getUsers, getPosts, getComments } from './requests/queries'
 // UTILS
 import { checkPropsExists } from '../utils/checks'
 
 const client = new ApolloBoost({ uri: 'http://localhost:4000' })
 
 describe('Queries', () => {
-  describe('Users', () => {
+  describe('getAllUsers', () => {
     test('Should expose public author profiles', async () => {
       const { data } = await client.query({ query: getUsers })
       checkPropsExists(data.users, _user.checkProps)
       expect(data.users.length).toBe(4)
+    })
+  })
+  
+  describe('getPosts', () => {
+    test('Should expose public author profiles', async () => {
+      const { data } = await client.query({ query: getPosts })
+      checkPropsExists(data.posts, _post.checkProps)
+      expect(data.posts.length).toBe(4)
+    })
+  })
+
+  describe('getAllComments', () => {
+    test('Should expose public author profiles', async () => {
+      const { data } = await client.query({ query: getComments })
+      checkPropsExists(data.comments, _comment.checkProps)
+      expect(data.comments.length).toBe(4)
     })
   })
 })
